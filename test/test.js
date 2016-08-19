06b2d9b471fe5aa5c2e73a900056aa86fa03c2aa
chipkeyes <chipkeyes@level11.com>
2016-08-19 13:35:35 -0700

    Set up basic test suite

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+var expect = chai.expect;
+var jsdom = require('mocha-jsdom')();
+var MapViewport;
+
+describe("Testing MapViewport", function() {
+    before(function() {
+        MapViewport = require('../src/MapViewport.js');
+    });
+
+    it("expect new instance to have correct height", function() {
+        var viewPort = new MapViewport(500, 500, {
+            width: 200,
+            height: 200,
+            path: '400.jpeg'
+        });
+        expect(viewPort.height).to.be.eql(500);
+    });
+});
